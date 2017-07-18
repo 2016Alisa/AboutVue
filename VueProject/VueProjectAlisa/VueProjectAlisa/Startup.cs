24fafe2d08c129f204f675ad0860be1fba65e5c6
2016Alisa <1229266765@qq.com>
2017-07-18 22:16:07 +0800

    vue1
    app1-app5

---
+++ VueProject/VueProjectAlisa/VueProjectAlisa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VueProjectAlisa.Startup))]
+namespace VueProjectAlisa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
